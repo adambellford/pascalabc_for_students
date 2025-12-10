9789855d8cfc28ca43b32a6d1405b637d47e2612
Adam Bellford <22466835+adambellford@users.noreply.github.com>
2025-12-10 19:46:34 +0200

    Chapter 2 finished

---
+++ for_students_1/chapter_02_linear_algs/task_2_15.pas
@@ -0,0 +1,13 @@
+﻿// Даны шесть натуральных чисел. Найти отношение минимального из средних арифме-
+// тических каждой пары к среднему геометрическому всех шести чисел.
+##
+var a, b, c, d, e, f: integer;
+Print('Введите шесть натуральных чисел:');
+Read(a, b, c, d, e, f);
+
+var geom_ave := (real(a) * b * c * d * e * f)**(1/6);
+var ab_ave := (a + b) / 2;
+var cd_ave := (c + d) / 2;
+var ef_ave := (e + f) / 2;
+var min_arif := Min(ab_ave, cd_ave, ef_ave);
+Print('Результат:', min_arif / geom_ave);
