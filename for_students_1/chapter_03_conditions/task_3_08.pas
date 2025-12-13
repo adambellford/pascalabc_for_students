ec2bdc6837d9e7bacb40c525942295fc94e9a5ba
Adam Bellford <22466835+adambellford@users.noreply.github.com>
2025-12-13 06:01:54 +0200

    Chapter 3 continued

---
+++ for_students_1/chapter_03_conditions/task_3_08.pas
@@ -0,0 +1,14 @@
+﻿// Вычислить значение кусочно-заданной функции
+##
+var x := ReadReal('x =');
+Print('y =',
+  if x <= -12 then
+    -2 * x - 23
+  else if x in -12..-6 then
+    Sqrt(36 - Sqr((x + 6))) + 1
+  else if x in -6..3 then
+    -Sqrt(81 - Sqr((x - 3))) + 7
+  else if x in 3..6 then
+    5 * x / 3 - 7
+  else
+    3)
