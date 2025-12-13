5d39b9c08ae24ec1ae4bf7d73d4c543d0409eb49
adambellford <22466835+adambellford@users.noreply.github.com>
2025-12-13 17:40:46 +0200

    3.18 and previous

---
+++ for_students_1/chapter_03_conditions/task_3_16.pas
@@ -0,0 +1,12 @@
+﻿// Даны три отрезка прямой длиной a, b и c. Можно ли из них составить
+// треугольник?
+##
+var (a, b, c) := ReadReal3('Введите длины сторон треугольника:');
+var first := (a + b) > c;
+var second := (b + c) > a;
+var third := (c + a) > b;
+
+if first and second and third then
+  Print('Треугольник возможен')
+else
+  Print('Треугольник не построить')
