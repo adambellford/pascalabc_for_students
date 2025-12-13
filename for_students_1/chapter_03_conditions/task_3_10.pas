9034f58e3eae50fb04f6a426496a05624d4a8750
Adam Bellford <22466835+adambellford@users.noreply.github.com>
2025-12-13 09:09:24 +0200

    3.11

---
+++ for_students_1/chapter_03_conditions/task_3_10.pas
@@ -0,0 +1,19 @@
+﻿//
+##
+var n := ReadInteger('Введите четырехзначное число:');
+var t, s, d, e: integer;
+t := n div 1000;
+s := n div 100 mod 10;
+d := n div 10 mod 10;
+e := n mod 10;
+if (t = 2) or (t = 7) or
+  (s = 2) or (s = 7) or
+  (d = 2) or (d = 7) or
+  (e = 2) or (e = 7) then
+    Println('Двойка или семерка присутствует')
+else
+  Println('Двойки или семерки нет');
+var b1 := (t = 1) or (s = 1) or (d = 1) or (e = 1);
+var b2 := (t = 6) or (s = 6) or (d = 6) or (e = 6);
+if b1 and b2 then
+  Print('Единица и шестерка присутствуют');
---
+++ for_students_1/chapter_03_conditions/task_3_11.pas
@@ -0,0 +1,14 @@
+﻿// Дано целое пятизначное число. Если куб суммы его цифр кратен натуральному b,
+// вывести True, иначе False.
+##
+var n := ReadInteger('Введите пятизначное число');
+var b := ReadInteger('Введите число b');
+
+var cube_of_sum := ((n div 10000) + 
+            (n div 1000 mod 100) +
+            (n div 100 mod 10) +
+            (n mod 10))**3;
+if (integer(cube_of_sum) mod b = 0) then
+  Print('True')
+else
+  Print('False')
