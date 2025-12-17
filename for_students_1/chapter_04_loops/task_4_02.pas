c85d311fed34cc8d0f3bdc4e60a4b956d3955629
adambellford <22466835+adambellford@users.noreply.github.com>
2025-12-17 09:08:03 +0200

    4.02 factorial

---
+++ for_students_1/chapter_04_loops/task_4_02.pas
@@ -0,0 +1,11 @@
+﻿// Найти и вывести значения 5!, 10!, 20! и 100!
+##
+loop 4 do
+begin
+  var n := ReadInteger('n = ');
+  var p := 1.0;
+  
+  for var i := 1 to n do
+    p *= i;
+  Writeln(n, '! = ', p);
+end;
