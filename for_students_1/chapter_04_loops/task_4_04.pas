c015e518d27c6ea72b58fb442b915efa9d0949f3
adambellford <22466835+adambellford@users.noreply.github.com>
2025-12-17 10:37:12 +0200

    4.04

---
+++ for_students_1/chapter_04_loops/task_4_04.pas
@@ -0,0 +1,23 @@
+﻿// Среди целых чисел из интервала [a;b] выбрать те, которые кратны
+// трем и оканчиваются цифрой 9. Найти сумму отобранных
+// чисел и их среднее значение
+##
+var (a, b) := ReadInteger2('Введите границы интервала [a, b]:');
+if a >= b then
+begin
+  Print('Неверные границы интервала');
+  exit
+end;
+var (s, avg) := (0, 0.0);
+var good_nums := 0;
+for var i := a to b do
+begin
+  if (i mod 3 = 0) and (Abs(i) mod 10 = 9) then
+  begin
+    good_nums += 1;
+    s += i;
+  end;
+end;
+avg := s / good_nums;
+Println('Сумма:', s);
+Print('Среднее:', avg);
