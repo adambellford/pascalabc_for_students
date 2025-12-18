7f938c31a7fe4cc58c67ac7afc918bb37ddb7f26
adambellford <22466835+adambellford@users.noreply.github.com>
2025-12-18 16:06:00 +0200

    4.09

---
+++ for_students_1/chapter_04_loops/task_4_07.pas
@@ -0,0 +1,8 @@
+﻿// Вычислить сумму
+##
+var sum := 1.0;
+// var znak := 1;
+for var i := 2 to 25 do
+  sum += ((i + 1) mod 2 = 0 ? 1 : -1) * (1 / i); // * znak
+  // znak := -znak;
+Print('Сумма:', sum)
