e44a986bd31d43d686e490370260d484d3fb7479
adambellford <22466835+adambellford@users.noreply.github.com>
2025-12-19 17:35:32 +0200

    4.15

---
+++ for_students_1/chapter_04_loops/task_4_14.pas
@@ -0,0 +1,19 @@
+﻿// Дано натуральное число. Верно ли, что сумма его четных цифр
+// больше, чем сумма нечетных?
+##
+var (sEven, sOdd) := (0, 0);
+var n := Abs(ReadInteger('Введите натуральное число:'));
+while n > 0 do
+begin
+  var d := n mod 10;
+  n := n div 10;
+  if d.IsEven then
+    sEven += d
+  else
+    sOdd += d
+end;
+
+if sEven > sOdd then
+  Print('Верно, сумма четных больше')
+else
+  Print('Неверно, сумма четных не больше')
---
+++ for_students_1/chapter_04_loops/task_4_15.pas
@@ -0,0 +1,24 @@
+﻿// Дано натуральное число. Верно ли, что в нем заданная цифра
+// встречается более одного раза?
+##
+var n := Abs(ReadInteger('Введите натуральное число:'));
+var k := ReadInteger('Заданная цифра:');
+
+var counter := 0;
+
+while n > 0 do
+begin
+  var d := n mod 10;
+  
+  if d = k then
+    counter += 1;
+  if counter >= 2 then
+    break;
+  
+  n := n div 10;
+end;
+
+if counter > 1 then
+  Print('Верно')
+else
+  Print('Неверно')
