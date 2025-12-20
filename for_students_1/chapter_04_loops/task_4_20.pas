6d12fe802e90b0f02968856fdc20f36d05a3a7ee
Adam Bellford <22466835+adambellford@users.noreply.github.com>
2025-12-20 17:20:30 +0200

    4.20 tries

--- for_students_1/chapter_04_loops/task_4_20.pas
+++ for_students_1/chapter_04_loops/task_4_20.pas
@@ -1,8 +1,16 @@
 ﻿// Найти НОК трех чисел a, b и c, введенных с клавиатуры
 ##
 var (a, b, c) := ReadInteger3;
+var (a1, b1, c1) := (a, b, c);
 while b <> 0 do
 begin
   (a, b) := (b, a mod b);
 end;
-a := (a * b) / 
+b := (a1 * b1) / real(a);
+b1 := b;
+while c <> 0 do
+begin
+  (b, c) := (c, b mod c);
+end;
+var nok := (b1 * c1) / b;
+Print(nok)
