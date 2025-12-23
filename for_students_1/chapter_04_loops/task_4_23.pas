244f5aefde72fcf9a2a65336649aec54434b37f8
adambellford <22466835+adambellford@users.noreply.github.com>
2025-12-23 09:44:02 +0200

    bad 4.23

---
+++ for_students_1/chapter_04_loops/task_4_23.pas
@@ -0,0 +1,27 @@
+﻿// Получить сто первых простых чисел
+begin
+  var j := 2;
+  var IsPrime := False;
+  Print(j);
+  var counter := 0;
+  j += 1;
+  repeat
+    // Println('Testing j: ', j);
+    IsPrime := True;
+    for var i := 2 to (j - 1) do
+    begin
+      // Println(j, i);
+      if j mod i = 0 then
+      begin
+        IsPrime := False;
+        break
+      end;
+    end;
+    if IsPrime then
+    begin
+      Print(j);
+      counter += 1;
+    end;
+    j += 2;
+  until counter > 99;
+end.
