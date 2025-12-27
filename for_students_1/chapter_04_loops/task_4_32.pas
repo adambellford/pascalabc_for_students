cb6e90e91fade65bcefc3a01620c6635f84baab7
adambellford <22466835+adambellford@users.noreply.github.com>
2025-12-27 22:17:43 +0200

    test

--- for_students_1/chapter_04_loops/task_4_32.pas
+++ for_students_1/chapter_04_loops/task_4_32.pas
@@ -1,25 +1,21 @@
 ﻿// Найти все простые делители заданного числа
 ##
 var n := ReadInteger('n =');
-var iterations := 0;
 var i := 2;
-while n mod i = 0 do
+while n mod 2 = 0 do
 begin
-  iterations += 1;
-  Write(i, ' ');
-  n := n div i;
+  Write(2, ' ');
+  n := n div 2;
 end;
 
-var end_var := Trunc((Round(Sqrt(n) - 3) / 2));
-
-for var j := 0 to end_var do
-begin
-  var t := 3 + j * 2; 
-  while n mod t = 0 do
+i := 3;
+while i <= Round(Sqrt(n)) do
+  if n mod i = 0 then
   begin
-    iterations += 1;
-    Write(t, ' ');
-    n := n div t
-  end;
-end;
-Println('Iterations:', iterations)
+    Print(i);
+    n := n div i
+  end
+  else
+    i += 2;
+if n > 1 then
+  Print(n)
