6f233cb2b75aada426aaac75ba6115afbbd2ddbb
adambellford <22466835+adambellford@users.noreply.github.com>
2025-12-27 19:26:31 +0200

    4.30

---
+++ for_students_1/chapter_04_loops/task_4_30.pas
@@ -0,0 +1,9 @@
+﻿// Найти все без исключения делители заданного числа и вывести их
+// в порядке возрастания
+##
+var n := ReadInteger;
+for var i := 1 to n div 2 do
+begin
+  if n mod i = 0 then
+    Print(i);
+end;
