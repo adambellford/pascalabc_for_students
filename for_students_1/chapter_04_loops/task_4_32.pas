ee7583c93c0dfb670e09a62fc57dffc5299e5dd9
adambellford <22466835+adambellford@users.noreply.github.com>
2025-12-27 21:38:51 +0200

    4.32

---
+++ for_students_1/chapter_04_loops/task_4_32.pas
@@ -0,0 +1,12 @@
+﻿// Найти все простые делители заданного числа
+##
+var n := ReadInteger('n =');
+
+for var j := 2 to Round(Sqrt(n)) do
+begin
+  while n mod j = 0 do
+  begin
+    Write(j, ' ');
+    n := n div j
+  end;
+end;
