1a92f6203b1d8a6063d79a7126f733379a8af299
adambellford <22466835+adambellford@users.noreply.github.com>
2025-12-27 19:53:07 +0200

    4.31

---
+++ for_students_1/chapter_04_loops/task_4_31.pas
@@ -0,0 +1,22 @@
+﻿// Найти кол-во нечетных делителей заданного числа
+##
+var counter := 0;
+var n := ReadInteger('Введите число:');
+for var i := 1 to n div 2 do
+begin
+  if (n mod i = 0) and (i mod 2 <> 0) then
+    counter += 1;
+end;
+Print('Кол-во нечетных делителей:', counter)
+//
+//var n := ReadInteger('n=');
+//var k := 0;
+//for var i := 1 to Round(Sqrt(n)) do
+//begin
+//  if n mod i = 0 then
+//  begin
+//    k += i.isOdd ? 1 : 0;
+//    var t := n div i;
+//    k += t.isOdd and (t <> i) ? 1 : 0;
+//  end;
+//end;
