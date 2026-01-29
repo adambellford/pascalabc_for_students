df0efcb0149b66eb707c4248ee6c989e1f3d623c
adambellford <22466835+adambellford@users.noreply.github.com>
2026-01-29 23:38:52 +0200

    task 5.01

---
+++ for_students_2/chapter_05/task_5_01.pas
@@ -0,0 +1,8 @@
+﻿// Вычислить и вывести с 12 знак. пос. запятой значение x, по формуле
+function P(n: integer) := n + Sqrt(n);
+
+function F(m, n: integer) := P(m) / P(n);
+
+begin
+  Write(F(5, 7) + F(12, 8) + F(31, 2):0:12);
+end.
