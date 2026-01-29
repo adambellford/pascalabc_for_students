f125a68d91b401303580869be0fe1149262b4ecf
adambellford <22466835+adambellford@users.noreply.github.com>
2026-01-29 23:28:39 +0200

    Recursion+

--- for_students_2/chapter_05/Recursion.pas
+++ for_students_2/chapter_05/Recursion.pas
@@ -1,4 +1,6 @@
 ﻿function Fact(n: integer): real := n = 1 ? 1.0 : n * Fact(n - 1);
+// Alternative:
+// function Fact(n: integer): real := n <= 1 then 1.0 else n * Fact(n - 1);
 
 begin
   Print(Fact(5));
