ca1b3548338496b87a7c4b5cfe22e0e256e61f84
adambellford <22466835+adambellford@users.noreply.github.com>
2026-01-29 23:14:35 +0200

    First Procedure

---
+++ for_students_2/chapter_05/maxMinAvg.pas
@@ -0,0 +1,23 @@
+﻿procedure MyFirst(n: integer; var mn, mx: integer; var m: real);
+begin
+  (mn, mx) := (100, -100);
+  var s := 0;
+  loop n do
+  begin
+    var k := Random(-99, 99);
+    s += k;
+    if k < mn then
+      mn := k
+    else if k > mx then
+      mx := k
+  end;
+  m := s / n;
+end;
+
+begin
+  var Max, Min: integer;
+  var Avg: real;
+  MyFirst(10, Min, Max, Avg);   // вызов процедуры
+  WriteLn('min = ', Min, ' max = ', Max, ' avg = ', Avg);
+  
+end.
