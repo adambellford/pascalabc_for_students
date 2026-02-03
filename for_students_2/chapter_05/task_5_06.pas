47683b068f39e83ba0c5c1a993a574a96b835e5b
adambellford <22466835+adambellford@users.noreply.github.com>
2026-02-03 14:04:11 +0200

    task 5.06 Akkerman function recursive

---
+++ for_students_2/chapter_05/task_5_06.pas
@@ -0,0 +1,14 @@
+﻿// Функция Аккермана
+function Akk(n, m: integer): integer;
+begin
+  if n = 0 then
+    Result := m + 1
+  else if m = 0 then
+    Result := Akk(n - 1, 1)
+  else
+    Result := Akk(n - 1, Akk(n, m - 1));
+end;
+
+begin
+  Print(Akk(3, 4));
+end.
