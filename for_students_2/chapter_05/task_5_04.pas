2368128f1fe50e561f6e6423a810a2c74afd1146
adambellford <22466835+adambellford@users.noreply.github.com>
2026-02-03 13:53:06 +0200

    task 5.04

---
+++ for_students_2/chapter_05/task_5_04.pas
@@ -0,0 +1,23 @@
+﻿// Реализовать и протестировать процедуру Triangle...
+function TrianglePossible(a, b, c: real) := 
+  (a + b > c) and (a + c > b) and (c + b > a);
+  
+function Geron(a, b, c: real): real;
+begin
+  var p := (a + b + c) / 2;
+  Result := Sqrt(p * (p - a) * (p - b) * (p - c));
+end;
+
+procedure Triangle;
+begin
+  var (a, b, c) := ReadReal3('Введите три числа: ');
+  Print('Площадь треугольника равна: ');
+  if TrianglePossible(a, b, c) then
+    Print(Geron(a, b, c))
+  else
+    Print(0);
+end;
+
+begin
+  Triangle
+end.
